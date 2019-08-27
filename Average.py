be135536a757e5e2a54b0b63a7cca166b3a993c8
shallun <54306102+shallun@users.noreply.github.com>
2019-08-27 13:41:12 +0530

    Basic files

---
+++ Average.py
@@ -0,0 +1,5 @@
+a=int(input("enter the given value="))
+b=int(input("enter the given value="))
+c=int(input("enter the given value="))
+avg=(a+b+c)/3
+print(avg)
---
+++ boolean expressios.py
@@ -0,0 +1,5 @@
+a=bool("true")
+b=bool("true")
+
+
+      
